e3ed6882163481136405241238199f56ea1aaf20
kavincode0 <kav.sharma0291@gmail.com>
2023-08-23 00:39:53 +0530

    Express, Postman

--- Backend/npm.js
+++ Backend/npm.js
@@ -1,3 +1,5 @@
 // pip is th package manager for python, npm is the package manager for node, to install different packages,
 // versions(1(version).0(added functions).0(bugsfixed)) with their dependencies. 
 // npm i/install package_name
+
+// Express is a web application framework for nodejs, custom backend, routing etc.
