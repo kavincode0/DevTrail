015e296125b14127a3a686bcc793e1925895fb98
kavincode0 <kav.sharma0291@gmail.com>
2023-08-28 01:34:10 +0530

    new sample

---
+++ Backend/SampleBackend/app.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const path = require("path");
+const fs = require("fs")
+const app = express()
+const port = 80
+
+// Express specific
+app.use(express.static('static'))//serving static files
+app.use(express.urlencoded())
+
+// Pug specific
+app.set('view engine', 'pug')//set the template engine as pug
+app.set('views', path.join(__dirname, 'views'))//set the views directory
+
+//Endpoints
+app.get('/', (req, res)=>{
+    // const params = {'title':""}
+ res.status(200).render('index.pug');
+})
+app.post('/', (req, res)=>{
+    console.log(req.body)
+    // const params = {}
+    res.status(200).render('index.pug');
+})
+
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`)
+}) 
